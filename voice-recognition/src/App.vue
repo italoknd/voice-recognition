1551a97d0b63b41a700faa4cc7e926be93ec949a
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-01-22 16:44:02 -0400

    refactotoring the component

--- voice-recognition/src/App.vue
+++ voice-recognition/src/App.vue
@@ -1,59 +1,11 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue'
-import Mic from './Mic.vue'
-
-const transcript = ref('')
-const is_recording = ref(false)
-// const clean_command = ref(["limpar tudo", "limpar texto", "apagar texto"]);
-
-const Recognition = window.SpeechRecognition || window.webkitSpeechRecognition //browsers methods that recognizes human speechs
-const sr = new Recognition()
-
-onMounted(() => {
-  sr.continuous = true
-  //this command tell us that the component will be listening us till we stop it
-  //it's like a watcher, will be there waiting for some voice to come on
-  sr.interimResults = true
-  //this one will take the results of the speech (but this is not the final result)
-
-  //starts the recording
-  sr.onstart = () => {
-    is_recording.value = true
-  }
-
-  //stops the recording
-  sr.onend = () => {
-    is_recording.value = false
-  }
-
-  sr.onresult = event => {
-    console.log(event);
-    const text = Array.from(event.results)
-      .map(res => res[0])
-      .map(res => res.transcript)
-      .join('')
-
-    transcript.value = text
-  }
-})
-
-// watch(transcript, () =>
-//   clean_command.value.map(item =>
-//     transcript.value.includes(item) ? (transcript.value = "") : ""
-//   )
-// );
-
-const toggle_mic = () => {
-  if (is_recording.value) sr.stop()
-  else sr.start()
-}
+import VueVoiceRecognition from './components/VueVoiceRecognition.vue'
 </script>
 
 <template>
   <div class="app">
     <div class="d-flex justify-content-center">
-      <Mic class="m-5" @click="toggle_mic()" />
-      <p class="text-box mt-2" v-text="transcript"></p>
+      <VueVoiceRecognition />
     </div>
   </div>
 </template>
