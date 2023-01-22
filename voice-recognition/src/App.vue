301acd231ea0cf6555e7846f7d5655e38c204d0b
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-01-22 16:46:25 -0400

    refactoring css

--- voice-recognition/src/App.vue
+++ voice-recognition/src/App.vue
@@ -9,23 +9,3 @@ import VueVoiceRecognition from './components/VueVoiceRecognition.vue'
     </div>
   </div>
 </template>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}
-
-#app {
-  max-width: 100vw;
-  max-height: 100vh;
-}
-
-body {
-  background-image: linear-gradient(90deg, #3960ec, rgb(0, 153, 255));
-  color: white;
-}
-</style>
